e163153c2835d1d20ec720d666312d0d51774b8d
Henrique Bergamo Shigihara <51368609+shigihenrique@users.noreply.github.com>
2020-04-27 18:18:15 -0300

    Update fizzbuzz.c

--- FizzBuzz/fizzbuzz.c
+++ FizzBuzz/fizzbuzz.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main()
+int main ()
 {
     int i;
-    for(i = 1; i < 101; i++)
+    for (i = 1; i < 101; i++)
     {
-        if((i % 3 == 0) && (i % 5 == 0))
+        if ((i % 3 == 0) && (i % 5 == 0))
         {
             printf("FizzBuzz\n");
         }
-        else if(i % 3 == 0)
+        else if (i % 3 == 0)
         {
             printf("Fizz\n");
         }
-        else if(i % 5 == 0)
+        else if (i % 5 == 0)
         {
             printf("Buzz\n");
         }
